fbbaa3db6c9be38b79fe5c43b2e86f821fe87ce2
Pierce Lally <pierce@vulpes.dev>
2023-11-07 11:36:54 -0500

    add wrap for sammys text screen (#1)
    * add wrap for sammys text screen
    
    * whitespace
    
    * remove newline

--- lua/textscreen_logger/loggers/sammyservers_textscreen.lua
+++ lua/textscreen_logger/loggers/sammyservers_textscreen.lua
@@ -1,4 +1,6 @@
-TEXTSCREENLOGGER.loggers["sammyservers_textscreen"] = function( ent )
+local class = "sammyservers_textscreen"
+
+TEXTSCREENLOGGER.loggers[class] = function( ent )
     if not ent.lines then return end
 
     local str = ""
@@ -11,4 +13,14 @@ TEXTSCREENLOGGER.loggers["sammyservers_textscreen"] = function( ent )
     local text = string.sub( str, 1, #str - 2 )
 
     return text
+end
+
+local ts = scripted_ents.GetStored( class ).t
+ts._SetLine = ts._SetLine or ts.SetLine
+
+ts.SetLine = function( self, line, text, ... )
+    local owner = self:CPPIGetOwner()
+    TEXTSCREENLOGGER.log( owner, class, text )
+    hook.Run( "TSLScreenSpawned", self, owner, class, text )
+    return ts._SetLine( self, line, text, ... )
 end
