1beb805fc95852f297d7d19024a72a81b0f4ed5a
Redox <mail@redox.to>
2023-11-08 17:09:44 +0100

    Refactor logic (#2)

--- lua/textscreen_logger/loggers/gmod_wire_textscreen.lua
+++ lua/textscreen_logger/loggers/gmod_wire_textscreen.lua
@@ -1,5 +1,20 @@
-TEXTSCREENLOGGER.loggers["gmod_wire_textscreen"] = function( ent )
-    if not ent.text then return end
+local class = "gmod_wire_textscreen"
+local ent = scripted_ents.GetStored( class )
+if not ent then return end
 
-    return ent.text
+local ts = ent.t
+ts._TriggerInput = ts._TriggerInput or ts.TriggerInput
+
+function ts:TriggerInput( name, value )
+    if name ~= "String" then
+        ts:_TriggerInput( name, value )
+    end
+
+    timer.Simple( 0, function()
+        if not IsValid( self ) then return end
+        local owner = self:CPPIGetOwner()
+        TEXTSCREENLOGGER.Log( self, owner, class, value )
+    end )
+
+    return ts._TriggerInput( self, name, value )
 end
--- lua/textscreen_logger/loggers/sammyservers_textscreen.lua
+++ lua/textscreen_logger/loggers/sammyservers_textscreen.lua
@@ -1,26 +1,14 @@
 local class = "sammyservers_textscreen"
+local ent = scripted_ents.GetStored( class )
+if not ent then return end
 
-TEXTSCREENLOGGER.loggers[class] = function( ent )
-    if not ent.lines then return end
-
-    local str = ""
-    for _, lineTbl in pairs( ent.lines ) do
-        if lineTbl.text ~= "" then
-            str = str .. lineTbl.text .. ", "
-        end
-    end
-
-    local text = string.sub( str, 1, #str - 2 )
-
-    return text
-end
-
-local ts = scripted_ents.GetStored( class ).t
+local ts = ent.t
 ts._SetLine = ts._SetLine or ts.SetLine
 
-ts.SetLine = function( self, line, text, ... )
+function ts:SetLine( line, text, ... )
+    if text == "" then return ts._SetLine( self, line, text, ... ) end
+
     local owner = self:CPPIGetOwner()
-    TEXTSCREENLOGGER.log( owner, class, text )
-    hook.Run( "TSLScreenSpawned", self, owner, class, text )
+    TEXTSCREENLOGGER.Log( self, owner, class, text )
     return ts._SetLine( self, line, text, ... )
 end
