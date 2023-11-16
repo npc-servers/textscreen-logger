ed4fe40816c2b4235f7b87dec35605e9465822c8
Redox <mail@redox.to>
2023-11-17 00:20:17 +0100

    Fix textscreen error

--- lua/textscreen_logger/loggers/sammyservers_textscreen.lua
+++ lua/textscreen_logger/loggers/sammyservers_textscreen.lua
@@ -9,6 +9,10 @@ function ts:SetLine( line, text, ... )
     if text == "" then return ts._SetLine( self, line, text, ... ) end
 
     local owner = self:CPPIGetOwner()
+    if not owner then -- persistent props
+        return ts:_SetLine( line, text, ... )
+    end
+
     TEXTSCREENLOGGER.Log( self, owner, class, text )
-    return ts._SetLine( self, line, text, ... )
+    return ts:_SetLine( line, text, ... )
 end
